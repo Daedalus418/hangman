5c693b0014c9e81e852bf2ca5dc56b177d76e214
daedalus418 <morin.gaelle@gmail.com>
2016-12-23 12:31:29 +0100

    chances remaining counter added

--- js/app.js
+++ js/app.js
@@ -1,2 +1,2 @@
 angular.module('app', [])
-        .controller('hangmanController', hangmanController)
+        .controller('hangmanController', hangmanController);
--- js/controllers/hangmanController.js
+++ js/controllers/hangmanController.js
@@ -1,3 +1,7 @@
 function hangmanController () {
+  const dictionnary = ['Javascript'];
+
+  this.totalChance = 8;
+  this.failedTest = 5;
 
 }
